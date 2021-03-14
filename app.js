cc26d87ee096dd59b8ae43fd84efafd159f87bde
harshit1507 <harshit1507@yahoo.com>
2021-03-14 23:06:20 +0530

    Secrets

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");          // Mongo Encryption Technique
+// const md5 = require("md5");         //MD5 Encryption Technique
+// const bcrypt = require("bcrypt");        // Bcrypt Encryption Technique
+// const saltRounds = 10;                   // Bcrypt Encryption Technique with salt rounds
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -14,21 +22,72 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});             // Mongo Encryption Technique
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.get("/auth/google",
+    passport.authenticate("google", {scope: ["profile"]})
+);
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", {failureRedirect: "/login"}),
+    function(req, res){
+        res.redirect("/secrets");
+    }
+);
 
 app.get("/login", function(req, res){
     res.render("login");
@@ -38,29 +97,44 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
-app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-
-    newUser.save(function(err){
+app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
         if(err)
         {
             console.log(err);
         }
-        else 
+        else
         {
-            res.render("secrets");
+            if(foundUsers)
+            {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
         }
     });
 });
 
-app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
 
-    User.findOne({email: username}, function(err, foundUser){
+app.get("/submit", function(req, res){
+    if(req.isAuthenticated())
+    {
+        res.render("submit");
+    }
+    else
+    {
+        res.redirect("/login");
+    }
+});
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+
+    // console.log(req.user.id);
+
+    User.findById(req.user.id, function(err, foundUser){
         if(err)
         {
             console.log(err);
@@ -69,14 +143,106 @@ app.post("/login", function(req, res){
         {
             if(foundUser)
             {
-                if(foundUser.password === password)
-                {
-                    res.render("secrets");
-                }
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets")
+                });
             }
+        }
+    });
+});
+
+app.post("/register", function(req, res){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err)
+        {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else
+        {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            })
         }
     })
 });
+
+app.post("/login", function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
+
+// app.post("/register", function(req, res){
+
+//     bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+//         const newUser = new User({
+//             email: req.body.username,
+//             // password: req.body.password              // Mongo Encryption Technique outside bcrypt function
+//             // password: md5(req.body.password)            // MD5 Encryption Technique outside bcrpyt funciton
+//             password: hash
+//         });
+    
+//         newUser.save(function(err){
+//             if(err)
+//             {
+//                 console.log(err);
+//             }
+//             else 
+//             {
+//                 res.render("secrets");
+//             }
+//         });
+//     });
+    
+// });
+
+// app.post("/login", function(req, res){
+//     const username = req.body.username;
+//     // const password = req.body.password;              // Mongo Encryption Technique
+//     // password = md5(req.body.password);                  // MD5 Encryption Technique
+//     password = req.body.password;
+
+//     User.findOne({email: username}, function(err, foundUser){
+//         if(err)
+//         {
+//             console.log(err);
+//         }
+//         else
+//         {
+//             if(foundUser)
+//             {
+//                 // if(foundUser.password === password)              // for comparing mongo encryption and MD5 Encryption
+//                 // {
+//                 //     res.render("secrets");
+//                 // }
+
+//                 bcrypt.compare(password, foundUser.password, function(err, result){
+//                     if(result === true)
+//                     {
+//                         res.render("secrets");
+//                     }
+                    
+//                 });
+//             }
+//         }
+//     })
+// });
 
 app.listen(3000, function(){
     console.log("Server started on port 3000.");
